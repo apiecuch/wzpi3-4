333a8a33e20446596742126966b3ef06ca7c1036
GenMyModel <help@genmymodel.com>
2014-11-28 09:34:36 +0000

    commit by GenMyModel using genmymodel: v20141128_093435

---
+++ src/main/java/wzpi34/Main2.java
@@ -0,0 +1,33 @@
+package wzpi34;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Main2
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Main2(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void policz() {
+		// TODO implement me	
+	}
+	
+}
+
